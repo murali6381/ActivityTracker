e05179e325acb056bb1c49fbd834e18bfa24ccbf
murali6381 <murali6381@gmail.com>
2020-08-24 02:15:16 +0800

    First Push

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent implements OnInit, OnDestroy {
+
+  isAuth = false;
+  @Output() closeSidenav = new EventEmitter<void>();
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+  onClose() {
+    this.closeSidenav.emit();
+    
+  }
+
+  onLogout() {
+    this.onClose();
+    this.authService.logout();
+  }
+ 
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
+
+}
---
+++ src/app/training/current-training/current-training.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { StopTrainingComponent } from './stop-training.component'
+import { TrainingService } from '../training.service';
+
+@Component({
+  selector: 'app-current-training',
+  templateUrl: './current-training.component.html',
+  styleUrls: ['./current-training.component.css']
+})
+export class CurrentTrainingComponent implements OnInit {
+  progress=0;
+  timer: number;
+
+  constructor(private dialog: MatDialog, private trainingService: TrainingService) { }
+
+  ngOnInit(): void {
+    this.startOrResumeTimer();
+  }
+
+  startOrResumeTimer() {
+    const step = this.trainingService.getRunningExercise().duration / 100*1000;
+    console.log('step: ' + step);
+    this.timer = setInterval(() => { //can ignore this error since it returns a number
+      this.progress = this.progress+1;
+      if(this.progress >= 100) {
+        this.trainingService.completeExercise();
+        clearInterval(this.timer);
+      }
+    }, step);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+      if (result) {
+        this.trainingService.cancelExercise(this.progress);
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
+  }
+
+}
